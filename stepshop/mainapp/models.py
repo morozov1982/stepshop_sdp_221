671d9cf2396a31be0d58086e61e05c06cfb78f26
Александр Морозов <@itstep.loc>
2023-10-24 17:32:37 +0600

    lesson_05

--- stepshop/mainapp/models.py
+++ stepshop/mainapp/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Имя',
+        max_length=64,
+        unique=True,
+    )
+
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name='категория',
+    )
+
+    name = models.CharField(
+        verbose_name='Имя продукта',
+        max_length=128,
+    )
+
+    image = models.ImageField(
+        upload_to='product_images',
+        blank=True,
+        verbose_name='Изображение',
+    )
+
+    short_desc = models.CharField(
+        max_length=100,
+        blank=True,
+        verbose_name='Краткое описание',
+    )
+
+    description = models.TextField(
+        blank=True,
+        verbose_name='Описание',
+    )
+
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        default=0,
+        verbose_name='Цена',
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Количество',
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
